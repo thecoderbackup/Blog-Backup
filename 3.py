b20359177316a07ddea874eee0d058c69fd723a8
Rook <eanderea1@protonmail.com>
2024-06-07 18:55:57 -0300

    3.py

---
+++ 3.py
@@ -0,0 +1,11 @@
+"""
+Mesmo, após o por do sol no dia-a-dia, os dias repetidos, muitas coisas nunca irão mudar.
+
+Com exceção os sentimentos que se tornam mais distantes, e entre minhas milhares de palavras e sentimentos todas memorias queimando em minha mente, Será que um dia irei parar de sonhar eternamente?
+
+Tão vermelho quanto o por do sol, o amor da junção de vossas mãos.
+
+O amor que queima que nem minhas memórias.
+
+O vosso por do sol no dia-a-dia, que nunca irá mudar.
+"""
