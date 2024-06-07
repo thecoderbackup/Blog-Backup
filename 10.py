4a2dc9eca3ae81d91ec701e47615ae343e71076c
Rook <eanderea1@protonmail.com>
2024-06-07 19:02:08 -0300

    10.py

---
+++ 10.py
@@ -0,0 +1,11 @@
+"""
+A realidade simplória da verdade do ser humano.
+
+Questionavel se estar doente e com mente se apagando é algo realmente ruim, talvez finalmente esqueça de tudo que foi lhe contado.
+Lentamente essa realidade se torna um surrealismo, espaço e tempo se distorcem.
+
+Eu já não ligo para os que os outros me deram em questão de conhecimento e de fato perdi o interesse naqueles que não conheço.
+Como dizia Schopenhauer.:
+
+“We will gradually become indifferent to what goes on in the minds of other people when we acquire a knowledge of the superficial nature of their thoughts, the narrowness of their views and of the number of their errors. Whoever attaches a lot of value to the opinions of others pays them too much honor.”
+"""
