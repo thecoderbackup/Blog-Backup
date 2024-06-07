01dea89317076d69d51eeac106200d7e40837d11
Rook <eanderea1@protonmail.com>
2024-06-07 19:04:51 -0300

    14.py

---
+++ 14.py
@@ -0,0 +1,26 @@
+"""
+(Somente seres sociais são capazes de compreender matemática complexa.)
+
+Dês de quando criança eu amava chamar atenção, fazia coisas imprudentes na frente da minha família e me lembro que eu gostava de cair de bunda no chão quando tinha cerca de uns 4 anos de idade, cair de bunda no chão fazia as outras pessoas rir e por tanto eu fazia independentemente se tinha acuidade motora, eu forjava situações para que aquilo ocorra.
+
+Todos nós aprendemos na infância que precisamos constantemente sermos amados e aprovados, essa é uma das principais característica de uma entidade social.
+
+Durante o inicio do fundamental me tornava cada vez mais competitivo com os demais, pois se eu estivesse no topo do pódio das capacidades acadêmicas eu sabia que conseguiria a atenção necessária - hoje eu sei que independentemente se estivesse ou não, eu não conseguiria atenção que queria pois a realidade brasileira despreza e estereotipa esse tipo de coisa. Aqui não é o Japão aonde o estereotipo de aluno de notas altas é sociável alto e amado - , enquanto eu fazia minha jornada por conhecimento criava inúmeros alter-egos para que nunca me sentisse sozinho.
+
+Tempos após tempos eu lentamente percebia que tudo que tinha feito não iria valer mais apena, a atenção que uma vez eu quis ter nunca iria se tornar uma realidade e no fundamental pela primeira vez desistir desse ser. Mas do inicio ao fim dessa jornada finalmente tinha sido descoberto um novo amor um amor verdadeiro e perfeito e intangível e esse era amor de matemática e nunca mais saber por atenção e sim saber pelo saber e dos meus sentimentos imperfeitos de uma entidade social, finalmente haveria de nascer algo mais.
+
+- Porém não dá pra compreender matemática sem ser um ser social.
+
+Uma nova doutrina nasceu em minha mente a ao vislumbrar tal amor como entidade social decidi comunicar aos meus semelhantes.
+E dessa comunicação nasceu a minha compreensão e dessa vez juntava meus alter-egos novamente em fileiras de sala e eu como um professor e começava explicar com vocabulário simples e conciso para meus alter-egos não muito inteligentes e cada dia conseguiria mais e mais ter prática matemática e entender multifatoralmente e análisar a compreender verdadeiramente.
+
+- Matemática é uma linguagem.
+
+Uma linguagem nasce para se comunicar, e para aqueles que não querem se comunicar e participar do mundo da entidade sociais a linguagem não é necessária. Se escreve e se escreve para alguém, se fala para falar com alguém, se escuta é porque há algum interesse em alguém.
+
+Temos casos de pessoas que Savants induzidas como Jason Padgett que demonstram o que cérebro humano é capaz, ele obteve uma forma "sinestesia matemática" após concussão grave e descreve que é capaz de ver forma matemática em todos lugares. O inconsciente humano é claramente capaz de performar uma série de operações matemáticas complexas fazer integrações, transformações espaciais, operações de conjuntos, todas essas que são necessárias para interagir no espaço, sem sequer nunca saber ou ter qualquer compreensão matemática de fato, a mente humana ainda "hoje em dia" está a muito graus de complexidade de processamento maior que computadores modernos, Afinal até abelhas conseguem (https://www.sciencedaily.com/releases/2019/02/190206200358.htm).
+
+A mente já performa operações matemáticas complexas porém só o ser social tem necessidade de forjar uma linguagem para se comunicar, só o ser social tem prática e pratica porque gosta de falar e de ser didático e é capaz de transformar o conhecimento inconsciente em conhecimento consciente pois o mesmo de fato tem essa necessidade de ser amado. E todos aqueles que brincam de aprender uma linguagem e se comunicarem se dizendo anti-sociais são hipócritas que nunca olharam para seu próprio coração, afinal qual é a razão de ser de uma linguagem?
+
+- Somente seres sociais são capazes de compreender~!
+"""
