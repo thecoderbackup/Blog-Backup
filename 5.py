a1a37715e8278e97cde5f158c1c5a97ab1fa07ab
Rook <eanderea1@protonmail.com>
2024-06-07 18:57:32 -0300

    5.py

---
+++ 5.py
@@ -0,0 +1,731 @@
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+""""""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
+
+"""
+(Poema)
+
+Só sou herói da dor.
+Só sou mente invisível.
+A entidade que nunca será amada.
+O corpo que nunca será reconhecido.
+
+Mas espero minha vitória minuciosa.
+Espero a quebra de minha realidade.
+
+O dia do milagre, do corpo que será finalmente visto.
+"""
