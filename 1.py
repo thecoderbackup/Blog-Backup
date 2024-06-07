444368e54e5826af63f802fe63146e6b11415e8b
Rook <eanderea1@protonmail.com>
2024-06-07 18:54:01 -0300

    1.py

---
+++ 1.py
@@ -0,0 +1,14 @@
+"""
+(Texto criado em 2014)
+
+A sociedade é como uma maquina antiga, inflexível e circular que vive em ritmo perpetuo e invasivo.
+Tudo continua o mesmo independente das variáveis de todas emoções e problemas ou questionamentos, e temos sempre que viver o mesmo dia a dia fazer a mesmas coisas e percorrer os mesmo caminhos.
+Temos que viver todos os dias as mesmas rotas de emprego de estudo.
+Temos que constantemente agradar as mesmas pessoas para garantir que elas nos ajudem.
+Nós podemos estar aprendendo nada, ou produzindo nada, ou no pior estado emocional que seja.
+Temos que estar sempre ali na mentira que é ali que devemos estar...
+Quando me perguntam...
+- Tudo certo?
+No fim não importa se está certo ou não.
+Então porque perguntam?
+"""
